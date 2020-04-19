ac7a3aa8d7223a9d74a63092064cb79df3ea9e90
unknown <trujillorodriguezjavier@gmail.com>
2020-04-19 11:19:08 +0200

    percentages favicon

--- src/app/components/country-number/country-number.component.ts
+++ src/app/components/country-number/country-number.component.ts
@@ -1,14 +1,33 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-country-number',
   templateUrl: './country-number.component.html',
   styleUrls: ['./country-number.component.scss'],
 })
-export class CountryNumberComponent implements OnInit {
+export class CountryNumberComponent implements OnInit, OnChanges {
   @Input() country: any;
+  percentages: any;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.setPercentages();
+  }
+  ngOnChanges(): void {
+    this.percentages = {
+      deaths: Math.round((this.country.deaths / this.country.confirmed) * 100),
+      recovered: Math.round(
+        (this.country.recovered / this.country.confirmed) * 100
+      ),
+    };
+  }
+  setPercentages() {
+    this.percentages = {
+      deaths: Math.round((this.country.deaths / this.country.confirmed) * 100),
+      recovered: Math.round(
+        (this.country.recovered / this.country.confirmed) * 100
+      ),
+    };
+  }
 }
