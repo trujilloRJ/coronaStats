a86371d6786d6615afbf0907e1f1a8a2ee7d4cb9
unknown <trujillorodriguezjavier@gmail.com>
2020-04-28 10:48:27 +0200

    thumbnail update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,5 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  feature_cases = 'cases';
   ngOnInit() {}
 }
